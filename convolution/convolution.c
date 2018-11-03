49669074564976f7815e822b0349f8784bc8ae76
Thinhinane Ihadadene <et_ihadadene@esi.dz>
2018-11-03 17:01:31 +0100

    fixing bugs in convolution

--- convolution/convolution.c
+++ convolution/convolution.c
@@ -1,22 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <math.h>
+#include <unistd.h>
 #include <sys/time.h>
 #include <stdint.h>
+#include <omp.h>
+
+//constants
+#define ROWS 2000
+#define COLS 2000
+#define BASE 255
+
+//global vars
+#pragma declarations
+uint8_t src[ROWS][COLS][3];
+float kernel[3][3];
+uint8_t conv[ROWS][COLS][3];
+#pragma enddeclarations
+
+#include <unistd.h>
+#include <sys/time.h>
 #include <math.h>
-
-
-#ifdef PERFCTR
-#include "papiStdEventDefs.h"
-#include <papi.h>
-#include "papi_defs.h"
-#endif
 
 #ifdef TIME
 #define IF_TIME(foo) foo;
 #else
 #define IF_TIME(foo)
 #endif
+
+void init_array()
+{
+    int i, j, k;
+    srand(0);
+    for (i=0; i<ROWS; i++) {
+        for (j=0; j<COLS; j++) {
+            for (k=0; k<3; k++) {
+                src[i][j][k] = rand()%BASE;
+            }
+        }
+    }
+}
+
+void init_kernel(){
+  kernel[0][0]=0;kernel[1][0]=1.0f/5;kernel[2][0]=0;
+  kernel[0][1]=1.0f/5;kernel[1][1]=1.0f/5;kernel[2][1]=0;
+  kernel[0][2]=0;kernel[1][2]=1;kernel[2][2]=0;
+}
+
+
+void print_array()
+{
+  int i,j,k;
+  for (i=0; i<ROWS; i++) {
+      for (j=0; j<COLS; j++) {
+          for (k=0; k<3; k++) {
+              printf("%d,", conv[i][j][k]);
+          }
+          printf("\t");
+      }
+      printf("\n");
+  }
+}
 
 double rtclock()
 {
@@ -28,94 +72,42 @@ double rtclock()
     return(Tp.tv_sec + Tp.tv_usec*1.0e-6);
 }
 
-#ifdef HAS_DECLS
-#include "decls.h"
-#else
-#define ROWS 5000
-#define COLS 5000
-#define BASE 255
-#endif
-/* Define our arrays */
-#pragma declarations
-uint8_t src[ROWS][COLS][3];
-uint8_t conv[ROWS][COLS][3];
-float kernel[3][3];
-#pragma enddeclarations
+int main()
+{
+	 int q,w,cc,kq,kw;
+   double t_start, t_end;
+   float prod;
 
-#define __PLACE_TO_INSERT_FORWARD_DECLARATIONS
-
-void print_matrix(uint8_t a[ROWS][COLS][3]){
-  int i,j,k;
-  for(i=0;i<ROWS;i++)
-   for(j=0;j<COLS;j++)
-     {
-       for(k=0;k<3;k++)
-         printf("%d,", a[i][j][k]);
-        printf("\t");
-     }printf("\n");
-}
-
-int main(int argc, char * argv[]) {
-  long int i,j,c;
-  double t_start = 0.0, t_end = 0.0;
-  double Total = 0.0;
-
-#ifdef DEBUG
-   printf("Cols = %ld Rows= %ld \n", COLS,ROWS);
-#endif
-
-  /* Initialization */
-  srand(0); // seed with a constant value to verify results
-  for(i=0;i<ROWS;i++)
-   for(j=0;j<COLS;j++)
-     for(c=0;c<3;c++)
-       src[i][j][c]= rand()%BASE;
-  //kernel
-  kernel[0][0]=0;kernel[1][0]=1.0f/5;kernel[2][0]=0;
-  kernel[0][1]=1.0f/5;kernel[1][1]=1.0f/5;kernel[2][1]=1.0f/5;
-  kernel[0][2]=0;kernel[1][2]=1;kernel[2][2]=0;
-
-  IF_TIME(t_start = rtclock());
-  float prod;
+	init_array() ;
+  init_kernel();
+	IF_TIME(t_start = rtclock());
 
 #pragma scop
-for (int q = 0; q < ROWS - 2; q++) {
-    for (int w = 0; w < COLS - 2; w++) {
-        for (int cc = 0; cc < 3; cc++) {
-            prod = 0.0f;
-            for (int kq = 0; kq < 3; kq++) {
-                for (int kw = 0; kw < 3; kw++) {
-                    prod += src[q + kq][w + kw][cc] * kernel[kq][kw];
-                }
-            }
-            conv[q][w][cc] = prod;
+for (q = 0; q < ROWS- 2; q++) {
+    for (w = 0; w < COLS - 2; w++) {
+        for (cc = 0; cc < 3; cc++) {
+
+            conv[q][w][cc] = src[q][w][cc]*kernel[0][0]+src[q+1][w][cc]*kernel[1][0]+
+            src[q+2][w][cc]*kernel[2][0]+src[q][w+1][cc]*kernel[0][1]+
+            src[q+1][w+1][cc]*kernel[1][1]+
+            src[q+1][w+2][cc]*kernel[1][2]+src[q+2][w][cc]*kernel[2][0]+
+            src[q+2][w+1][cc]*kernel[2][1]+src[q+2][w+2][cc]*kernel[2][2];
         }
     }
 }
 #pragma endscop
 
-  IF_TIME(t_end = rtclock());
-  IF_TIME(fprintf(stdout, "time = %0.6lfs\n", t_end - t_start));
+	IF_TIME(t_end = rtclock());
+	IF_TIME(fprintf(stdout, "%0.6lfs\n", t_end - t_start));
 
+    if (fopen(".test", "r")) {
 #ifdef __MPI
-  if (my_rank == 0) {
-#endif
-
-#ifdef __MPI
-  }
-#endif
-
-  if (fopen(".test", "r")) {
-#ifdef __MPI
-    if (my_rank == 0) {
-      // print_matrix(out);
-    }
+        if (my_rank == 0) {
+          //  print_array();
+        }
 #else
-    // print_matrix(out);
+        //print_array();
 #endif
-  }
-
-  return 0;
+    }
+    return 0;
 }
-
-// icc -O3 -fp-model precise heat_1d_np.c -o op-heat-1d-np -lm
---
+++ convolution/decls.h
@@ -0,0 +1,6 @@
+#define ROWS 5000
+#define COLS 5000
+#define BASE 255
+uint8_t src[ROWS][COLS][3];
+uint8_t conv[ROWS][COLS][3];
+float kernel[3][3];
